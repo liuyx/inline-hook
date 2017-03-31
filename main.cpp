99cb92b6f5b37d0b2b58cbbbf71b74e2b61b056f
liuyx <lamoliu@lamoliu-PC0.tencent.com>
2017-03-31 13:38:15 +0800

    add arm & thumb support

--- main.cpp
+++ main.cpp
@@ -2,86 +2,71 @@
 #include <iostream>
 #include <cstring>
 #include <future>
-#include <mutex>
-#include <thread>
+#include <cstdio>
 #include <random>
-#include <ctime>
-#include <vector>
-#include <ctime>
-
-std::mutex g_mutex;
 
 void print(const char *s) {
-	std::lock_guard<std::mutex> lock(g_mutex);
-	puts(s);
+    std::cout << s << std::endl;
 }
 
 int my_strcmp(const char *s1, const char *s2) {
-	print("hahahahah,it's been hooked");
-	return 0;
+    print("hahahahah,it's been hooked");
+    unhook((void *)strcmp);
+    std::cout << strcmp(s1,s2) << std::endl;
+    return 0;
 }
 
 int my_strlen(const char *s) {
-	return 11;
+    return 11;
 }
 
 int do_strcmp(const char *s1, const char *s2) {
-	return strcmp(s1,s2);
+    return strcmp(s1,s2);
 }
 
 static int counter;
 
 void f() {
-	//static std::default_random_engine e(time(0));
-	//static std::uniform_int_distribution<unsigned> u(10,50);
-	if (counter++ & 1)
-	//	std::this_thread::sleep_for(std::chrono::milliseconds(u(e)));
-	//else
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-	const char *s1 = "hello";
-	const char *s2 = "world";
-	if (strcmp(s1,s2) == 0) {
-		print("in f equal");
-	} else {
-		print("in f not equal");
-	}
+    //static std::default_random_engine e(time(0));
+    //static std::uniform_int_distribution<unsigned> u(10,50);
+    if (counter++ & 1)
+        //	std::this_thread::sleep_for(std::chrono::milliseconds(u(e)));
+        //else
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    const char *s1 = "hello";
+    const char *s2 = "world";
+    if (strcmp(s1,s2) == 0) {
+        print("in f equal");
+    } else {
+        print("in f not equal");
+    }
 }
 
 int main() {
-	const char *s1 = "hello";
-	const char *s2 = "world";
+    const char *s1 = "hello";
+    const char *s2 = "world";
 
-	std::vector<std::thread> vec;
-	for (int i = 0; i < 10; i++) {
-		vec.push_back(std::thread(f));
-	}
+    if (hook((void *)strcmp,(void *)my_strcmp) < 0) {
+        perror("hook");
+        exit(1);
+    }
 
-	print("start hook in main thread");
-	if (hook((void *)strcmp,(void *)my_strcmp) < 0) {
-		perror("hook");
-		exit(1);
-	}
+    if (strcmp(s1,s2) == 0) {
+        print("equal");
+    } else {
+        print("not equal");
+    }
 
-	if (strcmp(s1,s2) == 0) {
-		print("equal");
-	} else {
-		print("not equal");
-	}
+    if (unhook((void *)strcmp) < 0) {
+        perror("unhook");
+        exit(1);
+    }
 
-	std::this_thread::sleep_for(std::chrono::seconds(1));
+    if (strcmp(s1,s2) == 0) {
+        print("equal");
+    } else {
+        print("not equal");
+    }
 
-	if (unhook((void *)strcmp) < 0) {
-		perror("unhook");
-		exit(1);
-	}
-	if (strcmp(s1,s2) == 0) {
-		print("equal");
-	} else {
-		print("not equal");
-	}
-
-	for (auto &t : vec)
-		t.join();
-
-	return 0;
+    return 0;
 }
